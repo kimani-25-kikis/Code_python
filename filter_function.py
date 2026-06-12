a6292363e67c4ae236db5fc77fdf313c21384f62
kimani-25-kikis <kimanikikis@gmail.com>
2026-06-13 00:01:02 +0300

    Filter function

---
+++ filter_function.py
@@ -0,0 +1,7 @@
+words = ['tree', 'sky', 'mountain', 'river', 'cloud', 'sun']
+
+def is_long_word(word):
+    return len(word) > 4
+
+long_words = list(filter(is_long_word, words))
+print(long_words)
