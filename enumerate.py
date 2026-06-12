efe4c2e15731b3979d96a9035c890563d0c5e800
kimani-25-kikis <kimanikikis@gmail.com>
2026-06-12 22:53:29 +0300

    enumerate

---
+++ enumerate.py
@@ -0,0 +1,8 @@
+# languages = ['Spanish', 'English', 'Russian', 'Chinese']
+
+# list(enumerate(languages))
+
+languages = ['Spanish', 'English', 'Russian', 'Chinese']
+
+for index, language in enumerate(languages):
+    print(f'Index {index} and language {language}')
