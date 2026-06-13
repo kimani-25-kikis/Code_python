78bbd1e4526d89efd0bd3da990506f447d41d2fd
kimani-25-kikis <kimanikikis@gmail.com>
2026-06-13 12:15:00 +0300

    Lambda

---
+++ map_function.py
@@ -0,0 +1,7 @@
+celsius = [0, 10, 20, 30, 40]
+
+def to_fahrenheit(temp):
+    return (temp * 9/5) + 32
+
+fahrenheit = list(map(to_fahrenheit, celsius))
+print(fahrenheit)
---
+++ sum_function.py
@@ -0,0 +1,11 @@
+# numbers = [5, 10, 15, 20]
+# total = sum(numbers)
+# print(total)
+
+# numbers = [5, 10, 15, 20]
+# total = sum(numbers, 10) # positional argument
+# print(total)
+
+numbers = [5, 10, 15, 20]
+total = sum(numbers, start=10) # keyword argument
+print(total)
