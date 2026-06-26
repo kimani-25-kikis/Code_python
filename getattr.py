fc7430b1e9f26872fa8a40ef91e0505e1440400a
kimani-25-kikis <kimanikikis@gmail.com>
2026-06-26 11:01:37 +0300

    Has and set attributes

--- getattr.py
+++ getattr.py
@@ -1,10 +1,20 @@
+# class Person: 
+#     def __init__(self, name, age): 
+#         self.name = name 
+#         self.age = age 
+
+# person = Person('John Doe', 30) 
+ 
+# print(getattr(person, 'name'))  
+# print(getattr(person, 'age'))  
+# print(getattr(person, 'city', 'Embu'))
+
 class Person: 
     def __init__(self, name, age): 
         self.name = name 
         self.age = age 
 
-person = Person('John Doe', 30) 
- 
-print(getattr(person, 'name'))  
-print(getattr(person, 'age'))  
-print(getattr(person, 'city', 'Embu'))
+person = Person('John Doe', 30)
+
+attr_name = input('Enter the attribute you want to see: ')
+print(getattr(person, attr_name, 'Attribute not found'))
