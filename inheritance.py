afd20fb85ecd1dce48e24958f3acb5a7cce389b1
kimani-25-kikis <kimanikikis@gmail.com>
2026-07-07 16:03:16 +0300

    Using super() function

--- inheritance.py
+++ inheritance.py
@@ -5,7 +5,14 @@ class Animal:
         return f'{self.name} makes a sound'
 class Dog(Animal):
     bark = 'woof! woof!! woof!!!'
+    
+    # def sound(self):
+    #     return f'{self.name} barks {self.bark}'
+    
+    def sound(self):
+        base = super().sound()
+        return f'{base}, then {self.name} barks {self.bark}'
 jack = Dog('Jack')
 
 print(jack.sound())
-print(jack.bark)
+# print(jack.bark)
