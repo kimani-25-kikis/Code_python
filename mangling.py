c771beae98e8552350a19dbd5c33e86cbc8bcbc2
kimani-25-kikis <kimanikikis@gmail.com>
2026-07-09 12:40:12 +0300

    mangling

---
+++ mangling.py
@@ -0,0 +1,11 @@
+class Parent:
+    def __init__(self):
+        self.__data = 'Parent data'
+
+class Child(Parent):
+    def __init__(self):
+        super().__init__()
+        self.__data = 'Child data'
+
+c = Child()
+print(c.__dict__)
